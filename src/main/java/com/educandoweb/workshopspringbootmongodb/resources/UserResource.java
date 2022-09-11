97fc6649208301f98996701c572d0eda8cd2bda0
FelipeFariasFCF <felipe.fariasfcf@outlook.com>
2022-09-11 19:19:41 -0400

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/educandoweb/workshopspringbootmongodb/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopspringbootmongodb/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.educandoweb.workshopspringbootmongodb.domain.Post;
 import com.educandoweb.workshopspringbootmongodb.domain.User;
 import com.educandoweb.workshopspringbootmongodb.dto.UserDTO;
 import com.educandoweb.workshopspringbootmongodb.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }

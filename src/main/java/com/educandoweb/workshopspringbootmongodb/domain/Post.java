04712cc2fc394bb133469b3ae24c5d33f01e60fc
FelipeFariasFCF <felipe.fariasfcf@outlook.com>
2022-09-11 19:46:06 -0400

    acrescentando comentario aos posts

--- src/main/java/com/educandoweb/workshopspringbootmongodb/domain/Post.java
+++ src/main/java/com/educandoweb/workshopspringbootmongodb/domain/Post.java
@@ -1,26 +1,30 @@
 package com.educandoweb.workshopspringbootmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.educandoweb.workshopspringbootmongodb.dto.AuthorDTO;
+import com.educandoweb.workshopspringbootmongodb.dto.CommentDTO;
 
 @Document
-public class Post implements Serializable{
+public class Post implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
-	
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -72,6 +76,14 @@ public class Post implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
